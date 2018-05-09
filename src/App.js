ef9ba18dd8b8f63f8084f1d1c54b42eaa4083741
JackPound <jackpound@live.com>
2018-05-09 10:45:45 -0700

    installed some npm and updated App.js for validation of tokens

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
+import { browserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main'
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state = {
+			user: null
+		}
+	}
+	componentDidMount = () => {
+		this.getUser();
+	}
+	// Checking if there is a token in local storage / set user based on token
+	getUser = () => {
+		var token = localStorage.getItem('mernToken')
+		if(token){
+			axios.post('/auth/me/from/token', {
+				headers: { 'Authorization': `Bearer ${token}`}
+			}).then(response => {
+				this.setState({user: response.data.user})
+			}).catch(err => {
+				localStorage.removeItem('mernToken');
+				this.setState({user: null})
+			});
+		} else {
+			localStorage.removeItem('mernToken');
+			this.setState({user: null})
+		}
+	}
+	render() {
+	    return (
+			<div className="App">
+				<Header />
+				<Main />
+				<Footer />
+			</div>
+	    );
+	}
 }
 
 export default App;
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class Nav extends Component {
+	handleSubmit = (e) => {
+		e.preventDefault();
+		axios.get('http://localhost:3000/api/v1/games/')
+		.then(result => {
+			console.log('success', result);
+		})
+		.catch(err => {
+			console.log('error', err.response.data)
+		})
+	}
 	render() {
 		return(
-			<h1>this is NAV component</h1>
+			<div>
+				<h1>this is NAV component</h1>
+				<form onSubmit={this.handleSubmit}>
+					<input type="submit" />
+				</form>
+			</div>
 		)
 	}
 }
